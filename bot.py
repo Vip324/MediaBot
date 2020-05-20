ef296ee052ec0cdad30852f3d3a419c3269cbe7e
Vip324 <48558189+Vip324@users.noreply.github.com>
2020-05-20 16:55:59 +0300

    Add files via upload

--- bot.py
+++ bot.py
@@ -5,8 +5,8 @@ from src import config
 from src import parser
 from telebot import types
 
-bot = telebot.TeleBot('')
-spam_response = {} # Временная переменная хранит возвращаемые парсером данные
+bot = telebot.TeleBot('1224068014:AAGCOs8lO6eFb2VZBQwP49buOR3PfRfUAP8')
+spam_response = {}  # Временная переменная хранит возвращаемые парсером данные
 
 
 @bot.message_handler(commands=["start"])
@@ -21,7 +21,6 @@ def handle_help(message):
 
 @bot.callback_query_handler(func=lambda call: True)
 def callback(call):
-
     def show_parser(parser, film_source):
         response, photo = parser[film_source]['title'], parser[film_source]['img']
         if photo is not None:
@@ -44,42 +43,19 @@ def callback(call):
 
             # Выводим два варианта поиска
             if spam_response != {}:
-                if spam_response['parser_ivi'] != '':
-                    show_parser(spam_response,'parser_ivi')
-                if spam_response['parser_ivi'] != '':
-                    show_parser(spam_response, 'parser_youtube')
-            # response, photo = spam_response['parser_ivi']['title'], spam_response['parser_ivi']['img']
-            # if photo is not None:
-            #     bot.edit_message_text(chat_id=call.message.chat.id, message_id=call.message.message_id,
-            #                           text="[​​​​​​​​​​​]({}) {}".format(photo, response), parse_mode='markdown',
-            #                           reply_markup=keyboard)
-            # else:
-            #     bot.edit_message_text(chat_id=call.message.chat.id, message_id=call.message.message_id,
-            #                           text=response, reply_markup=keyboard)
-
-        if call.data == "show_less":
-            callback_button = types.InlineKeyboardButton(
-                text="Узнать больше", callback_data="show_more")
-            watch_button = types.InlineKeyboardButton(
-                text="Смотреть онлайн", callback_data="watch")
-            keyboard = types.InlineKeyboardMarkup()
-            keyboard.add(callback_button)
-            keyboard.add(watch_button)
-
-            # Выводим два варианта поиска
-            if spam_response != {}:
+                #
                 if spam_response['parser_ivi'] != '':
                     show_parser(spam_response, 'parser_ivi')
-                if spam_response['parser_ivi'] != '':
+                if spam_response['parser_youtube'] != '':
                     show_parser(spam_response, 'parser_youtube')
-            # response, photo = spam_response['parser_ivi']['title'], spam_response['parser_ivi']['img']
-            # if photo is not None:
-            #     bot.edit_message_text(chat_id=call.message.chat.id, message_id=call.message.message_id,
-            #                           text="[​​​​​​​​​​​]({}) {}".format(photo, response), parse_mode='markdown',
-            #                           reply_markup=keyboard)
-            # else:
-            #     bot.edit_message_text(chat_id=call.message.chat.id, message_id=call.message.message_id,
-            #                           text=response, reply_markup=keyboard)
+            response, photo = spam_response['parser_ivi']['title'], spam_response['parser_ivi']['img']
+            if photo is not None:
+                bot.edit_message_text(chat_id=call.message.chat.id, message_id=call.message.message_id,
+                                      text="[​​​​​​​​​​​]({}) {}".format(photo, response), parse_mode='markdown',
+                                      reply_markup=keyboard)
+            else:
+                bot.edit_message_text(chat_id=call.message.chat.id, message_id=call.message.message_id,
+                                      text=response, reply_markup=keyboard)
 
         if call.data == "watch":
             back_button = types.InlineKeyboardButton(
@@ -105,17 +81,24 @@ def callback(call):
 @bot.message_handler(content_types=["text"])
 def handle_text(message):
     spam_response = {}
-    response = parser.parser_text(message.json['text'])
-    spam_response = response
-    bot.send_message(message.chat.id, response)
+    # print(type(message.json['text']))
+    spam_response = parser.parser_text(message.json['text'])
+    # print(spam_response)
+
+    if spam_response['parser_youtube'] != '':
+        response = spam_response['parser_youtube']
+        bot.send_message(message.chat.id, 'We found on YouTube:')
+        bot.send_message(message.chat.id, response['title'])
+    if spam_response['parser_ivi'] != '':
+        response = spam_response['parser_ivi']
+        bot.send_message(message.chat.id, 'We found on IVI:')
+        bot.send_message(message.chat.id, response['title'])
 
 
 if __name__ == '__main__':
     while True:
         try:
             bot.polling(none_stop=True, )
-        except Exception as e:
-            logger.error(e)
+        except Exception as one:
+            logger.error(one)
             time.sleep(15)
-
-bot.polling()
