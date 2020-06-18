6ef2162282a273d1ad1a0d99b788757ba37b8aff
budda <buddazbs@yandex.ru>
2020-06-18 21:02:53 +0300

    Изменил сайт где искать текст

--- handlers/users/text.py
+++ handlers/users/text.py
@@ -18,17 +18,17 @@ async def handle_text(message: types.Message):
     bot.spam_response = parser.parser_text(message.from_user.id.json['text'])
 
     # готовим ответ по итогам поиска
-    if bot.spam_response['parser_youtube'] != '':
-        response = response + 'I found on YouTube: \n' + bot.spam_response['parser_youtube']['title'] + '\n \n'
-    if bot.spam_response['parser_ivi'] != '':
-        response = response + 'I found on IVI: \n' + bot.spam_response['parser_ivi']['title'] + '\n \n'
-    if bot.spam_response['parser_youtube'] == '' and bot.spam_response['parser_ivi'] == '':
-        response = config.ERR_MSG.format(message.from_user.id.json['text'])
+    if bot.spam_response['parser_film'] != '':
+        response = response + 'I found Film: \n' + bot.spam_response['parser_film']['title'] + '\n \n'
+        # if bot.spam_response['parser_ivi'] != '':
+        #     response = response + 'I found on IVI: \n' + bot.spam_response['parser_ivi']['title'] + '\n \n'
+        # if bot.spam_response['parser_film'] == '' and bot.spam_response['parser_ivi'] == '':
+        if bot.spam_response['parser_film'] == '':
+            response = config.ERR_MSG.format(message.from_user.id.json['text'])
 
     keyboard = types.InlineKeyboardMarkup()
     watch_button = types.InlineKeyboardButton(text='Просмотр', callback_data='watch')
     keyboard.add(watch_button)
-    video = ffmpeg_streaming.input(response)  # загоняем ссылку в обработчик и выводим видео
+    # video = ffmpeg_streaming.input(['link'])  # загоняем ссылку в обработчик и выводим видео
     # выводим ответ для пользователя с кнопками выбора
-    await message.answer(message.chat.id, video, reply_markup=keyboard)
-
+    await message.answer(message.chat.id, reply_markup=keyboard)
