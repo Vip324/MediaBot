8ce0c3eee1b05d626dd2fade95777b47ada2a0b5
Vip324 <48558189+Vip324@users.noreply.github.com>
2020-07-07 14:56:22 +0300

    добавил яви и поправил вывод

--- handlers/users/text.py
+++ handlers/users/text.py
@@ -19,23 +19,26 @@ async def handle_text(message: types.Message):
 
     # готовим ответ по итогам поиска
     if bot.spam_response['parser_film'] != '':
-        # response = response + SEARCH_MSG + '\n' \
-        #            + bot.spam_response['parser_film']['title'] + '\n' \
-        #            + WATCH_MSG + '\n'
         response = response + WATCH_MSG + '\n'
-        await message.answer(bot.spam_response['parser_film']['title'])
         await message.answer_photo(bot.spam_response['parser_film']['image'])
-        await message.answer(bot.spam_response['parser_film']['excerpt'])
 
-        # if bot.spam_response['parser_ivi'] != '':
-        #     response = response + 'I found on IVI: \n' + bot.spam_response['parser_ivi']['title'] + '\n \n'
-        # if bot.spam_response['parser_film'] == '' and bot.spam_response['parser_ivi'] == '':
+        await message.answer(bot.spam_response['parser_film']['title'])
+        await message.answer(bot.spam_response['parser_film']['link'])
+
+    if bot.spam_response['parser_ivi'] != '':
+        response = response + WATCH_MSG + '\n'
+        await message.answer_photo(bot.spam_response['parser_ivi']['image'])
+
+        await message.answer(bot.spam_response['parser_ivi']['title'])
+        await message.answer(bot.spam_response['parser_ivi']['link'])
+
+
     if bot.spam_response['parser_film'] == '':
         response = config.ERR_MSG.format(message['text'])
 
-    keyboard = types.InlineKeyboardMarkup()
-    watch_button = types.InlineKeyboardButton(text='Просмотр', callback_data='watch')
-    keyboard.add(watch_button)
+    # keyboard = types.InlineKeyboardMarkup()
+    # watch_button = types.InlineKeyboardButton(text='Просмотр', callback_data='watch')
+    # keyboard.add(watch_button)
     # video = ffmpeg_streaming.input(['link'])  # загоняем ссылку в обработчик и выводим видео
     # выводим ответ для пользователя с кнопками выбора
-    await message.answer(response, reply_markup=keyboard)
+    # await message.answer(response, reply_markup=keyboard)
